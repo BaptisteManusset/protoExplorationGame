ca4a5b072f89fd82416117c16ef06e16aef2b2a2
Baptiste Manusset <b.manusset@ldlc.com>
2022-10-08 14:44:42 +0200

    All package
    cinemachine
    dialogue system
    third person asset

---
+++ Assets/Plugins/Pixel Crushers/Dialogue System/Scripts/Options/Timeline/Playables/PlaySequence/PlaySequenceBehaviour.cs
@@ -0,0 +1,28 @@
+// Recompile at 07/10/2022 15:43:48
+
+#if USE_TIMELINE
+#if UNITY_2017_1_OR_NEWER
+// Copyright (c) Pixel Crushers. All rights reserved.
+
+using UnityEngine;
+using UnityEngine.Playables;
+using System;
+
+namespace PixelCrushers.DialogueSystem
+{
+
+    [Serializable]
+    public class PlaySequenceBehaviour : PlayableBehaviour
+    {
+
+        [Tooltip("Play this sequence.")]
+        [TextArea(5, 5)]
+        public string sequence;
+
+        [Tooltip("(Optional) The other subject in the sequence.")]
+        public Transform listener;
+
+    }
+}
+#endif
+#endif
